3cae6e8313f187e799c8fa1376e82739c2597bd4
Kishan Patel <kpatel@genospace.com>
2019-12-25 10:41:53 -0500

    add or reassign your vote

--- cabana/cabana/Views/ResponseView.swift
+++ cabana/cabana/Views/ResponseView.swift
@@ -12,10 +12,12 @@ struct ResponseView: View {
     var response: Response
     var room: Room
     var prompt: Prompt
+    var responseViewModel: ResponseViewModel
     init(room: Room, prompt: Prompt, response: Response) {
         self.response = response
         self.room = room
         self.prompt = prompt
+        self.responseViewModel = ResponseViewModel(room: room, prompt: prompt, response: response)
     }
     var body: some View {
         VStack {
@@ -30,7 +32,33 @@ struct ResponseView: View {
                 Text("\(response.votes.count)")
             }
         }.onTapGesture {
-            responseService.addVote(roomId: self.room.id, promptId: self.prompt.id, responseId: self.response.id!)
+            self.responseViewModel.addOrReassignVote()
+        }
+    }
+}
+
+public class ResponseViewModel: ObservableObject {
+    var response: Response
+    var room: Room
+    var prompt: Prompt
+    init(room: Room, prompt: Prompt, response: Response) {
+        self.response = response
+        self.room = room
+        self.prompt = prompt
+    }
+    
+    func addOrReassignVote() {
+        responseService.findResponseWithMyVote(roomId: room.id, promptId: prompt.id) { responseId in
+            if (responseId != nil) {
+                if (responseId! == self.response.id!) {
+                    return
+                }
+                // reassign vote
+                responseService.reassignVote(roomId: self.room.id, promptId: self.prompt.id, oldResponseId: responseId!, newResponseId: self.response.id!)
+                print("found response with my vote, \(responseId)")
+            } else {
+                responseService.addVote(roomId: self.room.id, promptId: self.prompt.id, responseId: self.response.id!)
+            }
         }
     }
 }
